2883c20284362c0a93df7628cf09d5dacd383bb0
Richard Padre <richardmpadre@gmail.com>
2018-10-25 14:48:24 +0800

    improved application layout and upgraded angular material to version 7

--- caterapp/ClientApp/src/app/ui/ui.module.ts
+++ caterapp/ClientApp/src/app/ui/ui.module.ts
@@ -5,17 +5,28 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { AppRoutingModule } from '../app-routing.module';
+import { MatButtonModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
     CommonModule,
     AppRoutingModule,
+    MatButtonModule,
     MatMenuModule,
+    MatToolbarModule,
   ],
   exports: [
     LayoutComponent,
     AppRoutingModule
   ],
-  declarations: [LayoutComponent, HeaderComponent, FooterComponent]
+  declarations: [
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  entryComponents: [
+    HeaderComponent
+  ]
 })
 export class UiModule { }
